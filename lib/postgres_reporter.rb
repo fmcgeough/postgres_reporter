aafb1909da0444c13d7581c337020df01573ea90
fmcgeough <fmcgeough@gmail.com>
2016-06-05 13:12:24 -0400

    Handle exceptions and set exit code properly
    Also added logging on error or misuse of the command line tool.

--- lib/postgres_reporter.rb
+++ lib/postgres_reporter.rb
@@ -3,5 +3,24 @@ require 'postgres_reporter/postgres_reader'
 
 # Provide easy means of writing report SQL to files
 module PostgresReporter
-  # Your code goes here...
+  # simple wrapper using PostgresReader
+  class PostgresReporter
+    # Read in the caller's provided options for connection to Postgres
+    # and setup the input and output file variables
+    def initialize(credentials_file, sql_file, output_file)
+      @options = YAML.load(File.read(credentials_file))['postgres_reporter']
+      @sql_file = sql_file
+      @output_file = output_file
+    end
+
+    # Execute the read and put CSV (with header) results in caller's
+    # specified file.
+    def query
+      rdr = PostgresReader.new(@options)
+      query = File.open(@sql_file, 'r', &:read)
+      File.open(@output_file, 'w') do |file|
+        rdr.query(query, file)
+      end
+    end
+  end
 end
